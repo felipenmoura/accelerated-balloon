6e6bed5ac913b2d1aa58490d445bc40a5bd6218c
Felipe N. Moura <felipenmoura@gmail.com>
2016-04-09 01:52:56 -0300

    Done

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,33 @@
 (()=>{
     
-    var balloon = document.querySelector('#balloon');
-    var lastZ = 0;
-    var zDiff = 2;
-    var tm = null;
-    var wip = 0;
-    var wipDivizor = 12;
+    var balloon = document.querySelector('#balloon'),
+        line = document.querySelector('.balloon--line'),
+        lastZ = 0,
+        lastY = 0,
+        minDiff = 2,
+        tm = null,
+        wip = 0,
+        wipDivizor = 12;
     
     function handleOrientation(event) {
+        
         var absolute = event.absolute;
         var alpha    = event.alpha;
         var beta     = event.beta;
         var gamma    = event.gamma;
         
-        if(Math.abs(lastZ - gamma) > zDiff){
+        if(Math.abs(lastZ - gamma) > minDiff){
             clearTimeout(tm);
             
             // telling the body about the direction for the animation
             if (gamma < 0) {
                 document.body.setAttribute('data-moving', 'right');
+                line.style.width = (-gamma * 1.1) + 'px';
+                line.style.transform = 'translateX(' + (gamma*.1) + 'px)';
             }
-            if (gamma < 0) {
+            if (gamma > 0) {
+                line.style.width = (gamma * 0.5) + 'px';
+                //line.style.transform = 'translateX(' + -(gamma*.2) + 'px)';
                 document.body.setAttribute('data-moving', 'left');
             }
             
@@ -34,14 +41,16 @@
             tm = setTimeout(_=>{
                 // ending the wipplash effect
                 balloon.style.transform = "rotateZ("+(-1*(gamma - wip))+"deg)";
-                document.body.removeAttribute('data-moving');
             }, 400);
             
             lastZ = gamma;
         }
         
-        // gatting farther or closer
-        balloon.style.width = (-beta + 200) + 'px';
+        if (Math.abs(lastY - beta) > minDiff) {
+            // gatting farther or closer
+            balloon.style.width = (-beta + 200) + 'px';
+            lastY = beta;
+        }
     }
 
     window.addEventListener("deviceorientation", handleOrientation, true);
